c29298e28c96651f4cfe5d97c9d5527118ba2be3
AniketChaudhary2007 <aniket.doc07@gmail.com>
2026-06-25 13:46:14 +0530

    day 14

---
+++ day 14/54.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, key, count = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to find frequency: ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key)
+            count++;
+    }
+
+    printf("Frequency of %d = %d", key, count);
+
+    return 0;
+}
